49b21c50c962f8afc6d62d785a541117c65f27cf
Konstantin Terziev <kd.terziev@gmail.com>
2016-10-01 18:30:58 +0100

    Arguments are being passed to the program.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let mut c = 1;
+    println!("Printing arguments passed:");
+    // Skip the first argument. It tells us the path to this executable.
+    for argument in std::env::args().skip(1) {
+        println!("{}: {}", c, argument);
+        c += 1;
+    }
 }
