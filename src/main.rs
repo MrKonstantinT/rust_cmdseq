7117146cf083b17217ab2811cfa6efd62e003afa
Konstantin Terziev <kd.terziev@gmail.com>
2016-10-04 20:38:26 +0100

    Program prints the usage comment and terminates as required.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 fn main() {
     let mut c = 1;
-    println!("Printing arguments passed:");
     // Skip the first argument. It tells us the path to this executable.
-    for argument in std::env::args().skip(1) {
+    let passed_arguments = std::env::args().skip(1);
+    if passed_arguments.len() <= 0 || passed_arguments.len() % 2 != 0 {
+        println!("Usage: cmdseq [-d <count dir>] <count1> <cmd1> [... <countn> <cmdn>]");
+        return; // terminate program.
+    }
+    println!("Printing arguments passed:");
+    for argument in passed_arguments {
         println!("{}: {}", c, argument);
         c += 1;
     }
