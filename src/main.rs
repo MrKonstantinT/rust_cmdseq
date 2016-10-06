f36e03294cbdfbbd0d059bde0fa7db9fdabfb5b1
Konstantin Terziev <kd.terziev@gmail.com>
2016-10-06 03:36:23 +0100

    Stuck with working with files.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::error::Error;
 use std::process::Command;
 use std::io::prelude::*;
 
@@ -38,13 +39,13 @@ fn collect_between_white(string: &str, start_white: usize, end_white: usize) -> 
 fn build_command(arguments: std::iter::Skip<std::env::Args>) -> String {
     let mut command = String::new();
     for argument in arguments {
-        command = if argument.contains(' ') { command + &"\"".to_string() + &argument + &"\" ".to_string() }
+        command = if argument.contains(' ') { command + "\"" + &argument + "\" " }
         else { command + &argument + &" ".to_string() };
     }
     String::from(command.trim_right())
 }
 
-fn load_cookie(directory: &str, to_hash: &str) { // Will return file handle in future.
+fn load_cookie(directory: &str, to_hash: &str) -> std::fs::File { // Will return file handle in future.
     // Select hash program here in the future.
     use std::process::Stdio;
     let hash_program = Command::new("sha256sum").stdin(Stdio::piped()).stdout(Stdio::piped()).spawn().expect("Failed to spawn sha256sum");
@@ -53,6 +54,21 @@ fn load_cookie(directory: &str, to_hash: &str) { // Will return file handle in f
     hash_program.stdout.unwrap().read_to_string(&mut hash_raw).expect("Failed to read output from sha256sum");
     let hash_extention: String = hash_raw.chars().take(16).collect();
     println!("File path: {}/cookie.{}", directory, hash_extention);
+    std::fs::create_dir_all(directory).expect("Failed to create directory(ies)");
+    let mut file_path = String::new();
+    file_path = file_path + directory + "/cookie." + &hash_extention;
+    let mut file = match std::fs::OpenOptions::new().read(true).write(true).create(true).open(&file_path) {
+        Ok(file) => file,
+        Err(why) => panic!("Failed to open file: {}\nReason: {}", file_path, why.description()),
+    };
+    let mut file_data = String::new();
+    file.read_to_string(&mut file_data).expect("Failed to read from our cookie.");
+    println!("Read from cookie: {}.", file_data);
+    if file_data == "" {
+        file.write(b"0").expect("Failed to initialse file data."); //
+        file.flush().expect("Failed to flush to disk.");
+    }
+    file
 }
 
 fn get_command_list(command: &str) -> Vec<CmdSeq>{
@@ -76,10 +92,13 @@ fn main() {
         return; // terminate program.
     }
     let mut user_command = build_command(passed_arguments);
+    let mut file_data = String::new();
     if user_command.starts_with("-d ") {
         let directory = &collect_between_white(&user_command, 1, 2);
         user_command = collect_between_white(&user_command, 2, 0); // Strip the this '-d' flag.
-        load_cookie(&directory, &user_command);
+        let file = &mut load_cookie(&directory, &user_command);
+        file.read_to_string(&mut file_data).expect("Failed to read from our cookie.");
+        println!("Read from cookie: {}.", file_data);
     } else { load_cookie("/tmp", &user_command); }
     for cmdseq in get_command_list(&user_command) {
         println!("CmdSeq\n  times_before_next: {}\n  cmd: {}", cmdseq.times_before_next, cmdseq.cmd);
