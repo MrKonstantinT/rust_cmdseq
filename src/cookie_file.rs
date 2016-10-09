5779412c61865ed9c15e030103160ff79b05ca5f
Konstantin Terziev <kd.terziev@gmail.com>
2016-10-09 19:14:01 +0100

    File module successfully replace previous code. Refactored main. Fix for #13

---
+++ src/cookie_file.rs
@@ -0,0 +1,58 @@
+// Module cookie_file
+use ::std::fs::{ File, OpenOptions };
+use ::std::error::Error;
+use ::std::process::{ Command, Stdio };
+use ::std::io::prelude::*;
+
+pub struct CookieFile {
+    cookie_path: String,
+}
+
+impl CookieFile {
+    pub fn new(directory: &str, command: &str) -> CookieFile {
+        let mut c_p = String::new();
+        let mut cookie = CookieFile { cookie_path: "Nil".to_string() };
+        c_p = c_p + directory + "/cookie." + &cookie.get_hash_extention(command);
+        cookie.cookie_path = c_p;
+        cookie.initialse_cookie();
+        cookie
+    }
+
+    fn initialse_cookie(&self) {
+        // Below we open the file but if it doesn't exist we create it automatically with read
+        // and write access.
+        let mut file = match OpenOptions::new().read(true).write(true).create(true).open(&self.cookie_path) {
+            Ok(file) => file,
+            Err(why) => panic!("Failed to open file: {}\nReason: {}", &self.cookie_path, why.description()),
+        };
+        let mut file_data = String::new();
+        file.read_to_string(&mut file_data).expect("Failed to read from our cookie.");
+        if file_data == "" { // If the file was newly created it initialse it.
+            file.write(b"0\n").expect("Failed to initialse file data.");
+        } // Leave it untouched if find out its not empty.
+        // Files are automatically closed when they go out of scope. Hooray!
+    }
+
+    fn get_hash_extention(&self, to_hash: &str) -> String {
+        let hash_program = Command::new("sha256sum").stdin(Stdio::piped()).stdout(Stdio::piped()).spawn().expect("Failed to spawn sha256sum");
+        hash_program.stdin.unwrap().write_all(to_hash.as_bytes()).expect("Failed to input into sha256sum");
+        let mut hash_raw = String::new();
+        hash_program.stdout.unwrap().read_to_string(&mut hash_raw).expect("Failed to read output from sha256sum");
+        hash_raw.chars().take(16).collect()
+    }
+
+    pub fn read_cookie(&self) -> usize {
+        let mut cookie = File::open(&self.cookie_path).expect("Could not open our cookie to read from it.");
+        let mut file_data = String::new();
+        cookie.read_to_string(&mut file_data).expect("Failed to read from our cookie.");
+        file_data.trim_right().parse().expect("Something went wrong with parsing the cookie contents.")
+    }
+
+    pub fn update_cookie(&self, content: usize) {
+        // Using create than open because it will truncate it for us and we won't have to worry
+        // about our buffer being too small.
+        let mut cookie = File::create(&self.cookie_path).expect("Could not open or truncate our cookie.");
+        let string = content.to_string() + "\n";
+        cookie.write(string.as_bytes()).expect("Failed to update the cookie.");
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,22 @@
-use std::error::Error;
 use std::process::Command;
-use std::io::prelude::*;
-use std::process::Stdio;
+use cmdseq::CmdSeq;
 use string_utils::{ build_command, collect_between_white };
+use cookie_file::CookieFile;
 
 mod cmdseq;
 mod string_utils;
+mod cookie_file;
 
-fn load_cookie(directory: &str, to_hash: &str) -> std::fs::File {
-    // Select hash program here in the future.
-    let hash_program = Command::new("sha256sum").stdin(Stdio::piped()).stdout(Stdio::piped()).spawn().expect("Failed to spawn sha256sum");
-    hash_program.stdin.unwrap().write_all(to_hash.as_bytes()).expect("Failed to input into sha256sum");
-    let mut hash_raw = String::new();
-    hash_program.stdout.unwrap().read_to_string(&mut hash_raw).expect("Failed to read output from sha256sum");
-    let hash_extention: String = hash_raw.chars().take(16).collect();
-    std::fs::create_dir_all(directory).expect("Failed to create directory(ies)");
-    let mut file_path = String::new();
-    file_path = file_path + directory + "/cookie." + &hash_extention;
-    let mut file = match std::fs::OpenOptions::new().read(true).write(true).create(true).open(&file_path) {
-        Ok(file) => file,
-        Err(why) => panic!("Failed to open file: {}\nReason: {}", file_path, why.description()),
-    };
-    let mut file_data = String::new();
-    file.read_to_string(&mut file_data).expect("Failed to read from our cookie.");
-    if file_data == "" {
-        file.write(b"0\n").expect("Failed to initialse file data.");
-        file.seek(std::io::SeekFrom::End(-2)).expect("Failed to make the file object read from the beginning.");
-    } else { file.seek(std::io::SeekFrom::End(-1 * file_data.len() as i64)).expect("Failed to make the file object read from the beginning."); }
-    file
+fn process_cookie(dir: &str, u_s: &str) -> (Vec<CmdSeq>, usize) {
+    let cookie = CookieFile::new(dir, u_s);
+    let index = cookie.read_cookie();
+    let (cmd_list, number_of_operations) = cmdseq::get_command_list(u_s);
+    if index + 1 >= number_of_operations { cookie.update_cookie(0); } // Perform 'wrap round':
+    else { cookie.update_cookie(index + 1); }
+    (cmd_list, index)
 }
 
-fn main() {
-    // Skip the first argument. It tells us the path to this executable.
-    let passed_arguments = std::env::args().skip(1);
-    if passed_arguments.len() <= 0 || passed_arguments.len() % 2 != 0 {
-        println!("Usage: cmdseq [-d <count dir>] <count1> <cmd1> [... <countn> <cmdn>]");
-        return; // terminate program.
-    }
-    let mut user_command = build_command(passed_arguments);
-    let mut directory = String::from("/tmp");
-    let mut file_data = String::new();
-    if user_command.starts_with("-d ") {
-        directory = collect_between_white(&user_command, 1, 2);
-        user_command = collect_between_white(&user_command, 2, 0); // Strip the this '-d' flag.
-    }
-    println!("{}", &user_command); // Show the user what the program will actually work with.
-    let file = &mut load_cookie(&directory, &user_command);
-    file.read_to_string(&mut file_data).expect("Failed to read from our cookie.");
-    file.seek(std::io::SeekFrom::End(-1 * file_data.len() as i64)).expect("Failed to make the file object read from the beginning.");
-    let index: usize = file_data.trim_right().parse().expect("Something went wrong with parsing the cookie contents.");
-    let (cmd_list, number_of_operations) = cmdseq::get_command_list(&user_command);
-    if index + 1 >= number_of_operations { // Perform 'wrap round':
-        file.set_len(0).expect("Failed to shirnk file to size 0.");
-        file.write(b"0\n").expect("Failed to update cookie.");
-    } else {
-        let next_index = index + 1;
-        let string = next_index.to_string() + "\n";
-        file.write(string.as_bytes()).expect("Failed to update cookie.");
-    };
+fn pick_command_and_execute(cmd_list: &Vec<CmdSeq>, index: usize) {
     let mut accumulator: usize = 0;
     for cmdseq in cmd_list {
         accumulator += cmdseq.get_times_before_next();
@@ -77,4 +36,22 @@ fn main() {
             break;
         }
     }
+}
+
+fn main() {
+    // Skip the first argument. It tells us the path to this executable.
+    let passed_arguments = std::env::args().skip(1);
+    if passed_arguments.len() <= 0 || passed_arguments.len() % 2 != 0 {
+        println!("Usage: cmdseq [-d <count dir>] <count1> <cmd1> [... <countn> <cmdn>]");
+        return; // terminate program.
+    }
+    let mut user_command = build_command(passed_arguments);
+    let mut directory = String::from("/tmp");
+    if user_command.starts_with("-d ") {
+        directory = collect_between_white(&user_command, 1, 2);
+        user_command = collect_between_white(&user_command, 2, 0); // Strip the this '-d' flag.
+    }
+    println!("{}", &user_command); // Show the user what the program will actually work with.
+    let (cmd_list, index) = process_cookie(&directory, &user_command);
+    pick_command_and_execute(&cmd_list, index);
 }
