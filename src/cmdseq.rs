d7c7c5c3139df4aeb967f6bfce2e677139c2ba19
Konstantin Terziev <kd.terziev@gmail.com>
2016-10-12 16:49:25 +0100

    Changed logic to use getopts. Semi fixed #16.

--- src/cmdseq.rs
+++ src/cmdseq.rs
@@ -23,21 +23,19 @@ impl CmdSeq {
     }
 }
 
-pub fn get_command_list(command: &str) -> (Vec<CmdSeq>, usize) {
+pub fn get_command_list(args: &Vec<String>) -> (Vec<CmdSeq>, usize) {
     let mut command_list: Vec<CmdSeq> = Vec::new();
     // The number of operations we would execute before we need to start from beginning.
     let mut num_operations: usize = 0;
     let mut num = 0;
-    while num <= count_white_space(command) {
-        // Using 'while' loop as 'step_by()' has issues atm.
-        let t_b_n: usize = collect_between_white(command, num, num + 1)
+    // Using 'while' loop as 'step_by()' has issues atm.
+    while num < args.len() {
+        let t_b_n: usize = args[num]
             .parse()
             .expect("Usage: cmdseq [-d <count dir>] <count1> <cmd1> [... <countn> <cmdn>]");
         num_operations += t_b_n;
-        command_list.push(CmdSeq::new(t_b_n,
-                                      collect_between_white(command, num + 1, num + 2)
-                                          .replace("\"", "")));
-        num = num + 2;
+        command_list.push(CmdSeq::new(t_b_n, args[num + 1].clone()));
+        num += 2;
     }
     (command_list, num_operations)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+extern crate getopts;
+
+use std::env;
+use getopts::Options;
 use std::process::Command;
 use cmdseq::CmdSeq;
 use string_utils::{build_command, collect_between_white};
@@ -7,10 +11,14 @@ mod cmdseq;
 mod string_utils;
 mod cookie_file;
 
-fn process_cookie(dir: &str, u_s: &str) -> (Vec<CmdSeq>, usize) {
-    let cookie = CookieFile::new(dir, u_s);
+fn print_usage() {
+    println!("Usage: cmdseq [-d <count dir>] <count1> <cmd1> [... <countn> <cmdn>]");
+}
+
+fn process_cookie(dir: &str, command_pairs: &str, args: &Vec<String>) -> (Vec<CmdSeq>, usize) {
+    let cookie = CookieFile::new(dir, command_pairs);
     let index = cookie.read_cookie();
-    let (cmd_list, number_of_operations) = cmdseq::get_command_list(u_s);
+    let (cmd_list, number_of_operations) = cmdseq::get_command_list(args);
     if index + 1 >= number_of_operations {
         cookie.update_cookie(0); // Perform 'wrap round'
     } else {
@@ -28,8 +36,7 @@ fn pick_command_and_execute(cmd_list: &Vec<CmdSeq>, index: usize) {
             let mut program = Command::new(match word_iter.next() {
                 Some(string) => string,
                 None => {
-                    println!("Usage: cmdseq [-d <count dir>] <count1> <cmd1> [... <countn> \
-                              <cmdn>]");
+                    print_usage();
                     return; // terminate program.
                 }
             });
@@ -45,18 +52,32 @@ fn pick_command_and_execute(cmd_list: &Vec<CmdSeq>, index: usize) {
 
 fn main() {
     // Skip the first argument. It tells us the path to this executable.
-    let passed_arguments = std::env::args().skip(1);
-    if passed_arguments.len() <= 0 || passed_arguments.len() % 2 != 0 {
-        println!("Usage: cmdseq [-d <count dir>] <count1> <cmd1> [... <countn> <cmdn>]");
-        return; // terminate program.
+    let passed_arguments: Vec<String> = env::args().skip(1).collect();
+    let mut opts = Options::new();
+    opts.optopt("d", "", "Sets the directory for the cookie file.", "DIR");
+    opts.optflag("h", "", "Prints this usage message.");
+    let option_matches = match opts.parse(&passed_arguments) {
+        Ok(o_match) => o_match,
+        Err(_) => panic!("Failed to parse options."),
+    };
+    if option_matches.opt_present("h") {
+        // Print usage for help flag.
+        print_usage();
+        return;
     }
-    let mut user_command = build_command(passed_arguments);
-    let mut directory = String::from("/tmp");
-    if user_command.starts_with("-d ") {
-        directory = collect_between_white(&user_command, 1, 2);
-        user_command = collect_between_white(&user_command, 2, 0); // Strip the this '-d' flag.
+    // Select directory for our cookies.
+    let cookie_directory = match option_matches.opt_str("d") {
+        Some(user_dir) => String::from(user_dir),
+        None => String::from("/tmp"),
+    };
+    // Ensure we have (num command) pairs.
+    if option_matches.free.len() % 2 != 0 {
+        print_usage();
+        return;
     }
-    println!("{}", &user_command); // Show the user what the program will actually work with.
-    let (cmd_list, index) = process_cookie(&directory, &user_command);
+    let free_args = option_matches.free.clone();
+    let command_pairs: String = free_args.join(" ");
+    println!("{}", command_pairs); // Show the user what the program will actually work with.
+    let (cmd_list, index) = process_cookie(&cookie_directory, &command_pairs, &free_args);
     pick_command_and_execute(&cmd_list, index);
 }
